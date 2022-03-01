61a57e6e483e5ef85942dca00e3bc44babbe0c97
Shadman Saqif <shadman.saqif@gmail.com>
2022-03-01 19:51:43 +0600

    connected postgres database and migrated models

--- TODO_LIST/TODO_LIST/settings.py
+++ TODO_LIST/TODO_LIST/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo_app',
 
 
     'rest_framework',
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'TODO_LIST.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ToDoList',
+        'USER':'postgres',
+        'PASSWORD':'12345',
+        'HOST':'127.0.0.1',
+        'PORT':'5432',
     }
 }
 
--- TODO_LIST/todo_app/models.py
+++ TODO_LIST/todo_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class todoList(models.Model):
+    task = models.CharField(max_length=100,blank=True,null=True)
+
+    def __str__(self):
+        return self.task
