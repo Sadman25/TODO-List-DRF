f3ba2c7ec0a47f019cd76b3a2daa94216f25d9fd
Shadman Saqif <shadman.saqif@gmail.com>
2022-03-01 23:19:11 +0600

    CRUD operations implemented using API views

--- TODO_LIST/todo_app/urls.py
+++ TODO_LIST/todo_app/urls.py
@@ -3,6 +3,9 @@ from .import views
 
 urlpatterns = [
     path('',views.taskList,name='taskList'),
+    path('taskCreate',views.taskCreate,name='taskCreate'),
     path('taskDetails/<str:pk>/',views.taskDetails,name='taskDetails'),
+    path('taskEdit/<str:pk>/',views.taskEdit,name='taskEdit'),
+    path('taskDelete/<str:pk>/',views.taskDelete,name='taskDelete'),
     
 ]
--- TODO_LIST/todo_app/views.py
+++ TODO_LIST/todo_app/views.py
@@ -18,3 +18,29 @@ def taskDetails(request,pk):
     tasks = todoList.objects.get(id=pk)
     serializer = taskSerializer(tasks, many=False)
     return Response(serializer.data)
+
+@api_view(['POST'])
+def taskCreate(request):
+    serializer = taskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def taskEdit(request,pk):
+    task = todoList.objects.get(id=pk)
+    serializer = taskSerializer(instance=task,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def taskDelete(request,pk):
+    task = todoList.objects.get(id=pk)
+    task.delete()
+
+    return Response("Item Successfully Deleted !!")
+
+    
