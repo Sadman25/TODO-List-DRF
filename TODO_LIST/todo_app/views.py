f043e65e1fd0fcb1d4fbd8b0abab637cc9245cc5
Shadman Saqif <shadman.saqif@gmail.com>
2022-03-02 16:07:19 +0600

    Status Code added and Updated

--- TODO_LIST/todo_app/views.py
+++ TODO_LIST/todo_app/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
+
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
@@ -9,38 +11,49 @@ from .models import todoList
 
 @api_view(['GET'])
 def taskList(request):
-    tasks = todoList.objects.all()
+    tasks = todoList.objects.all().order_by('-id')
     serializer = taskSerializer(tasks, many=True)
     return Response(serializer.data)
 
 @api_view(['GET'])
 def taskDetails(request,pk):
-    tasks = todoList.objects.get(id=pk)
-    serializer = taskSerializer(tasks, many=False)
-    return Response(serializer.data)
+    
+    try:
+        task = todoList.objects.get(id=pk)
+        serializer = taskSerializer(task, many=False)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 def taskCreate(request):
     serializer = taskSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
+        return Response(serializer.data,status=status.HTTP_201_CREATED)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
-    return Response(serializer.data)
-
-@api_view(['POST'])
+@api_view(['GET','POST'])
 def taskEdit(request,pk):
-    task = todoList.objects.get(id=pk)
-    serializer = taskSerializer(instance=task,data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-
-    return Response(serializer.data)
+    try:
+        task = todoList.objects.get(id=pk)
+        serializer = taskSerializer(instance=task,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+    except:
+        return Response("Item not Found",status=status.HTTP_400_BAD_REQUEST)
+    
 
 @api_view(['DELETE'])
 def taskDelete(request,pk):
-    task = todoList.objects.get(id=pk)
-    task.delete()
-
-    return Response("Item Successfully Deleted !!")
+    try:
+        task = todoList.objects.get(id=pk)
+        task.delete()
+        return Response("Item Successfully Deleted !!",status=status.HTTP_204_NO_CONTENT)
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     
