b42d0916c274a811b964dbbae85582deccd001de
Shadman Saqif <shadman.saqif@gmail.com>
2022-03-03 02:51:51 +0600

    Added Basic and Session Authentication

--- TODO_LIST/todo_app/views.py
+++ TODO_LIST/todo_app/views.py
@@ -5,10 +5,102 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 from .serializers import taskSerializer
 from .models import todoList
 # Create your views here.
+
+class taskCreateView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def post(self,request):
+        serializer = taskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class taskListView(APIView):
+    
+    def get(self,request):
+        tasks = todoList.objects.all().order_by('-id')
+        serializer = taskSerializer(tasks, many=True)
+        return Response(serializer.data)         
+    
+class taskDetailView(APIView):
+
+    def get_object(self,pk):
+        try:
+            return todoList.objects.get(id=pk)
+            # serializer = taskSerializer(task, many=False)
+            # return Response(serializer.data,status=status.HTTP_200_OK)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self,request,pk):
+        task = self.get_object(pk)
+        serializer = taskSerializer(task, many=False)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+
+class taskEditView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get_object(self,pk):
+        try:
+            return todoList.objects.get(id=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def put(self, request, pk, format=None):
+        task = self.get_object(pk)
+        serializer = taskSerializer(task, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class taskDeleteView(APIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+    def get_object(self,pk):
+        try:
+            return todoList.objects.get(id=pk)
+        except:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def delete(self, request, pk, format=None):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# class documentationCode(APIView):
+#     def get_object(self, pk):
+#         try:
+#             return todoList.objects.get(id=pk)
+#         except:
+#             raise Http404
+
+#     def get(self, request, pk, format=None):
+#         task = self.get_object(pk)
+#         serializer = taskSerializer(task)
+#         return Response(serializer.data)
+
+#     def put(self, request, pk, format=None):
+#         task = self.get_object(pk)
+#         serializer = taskSerializer(task, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk, format=None):
+#         task = self.get_object(pk)
+#         task.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
 
 # @api_view(['GET'])
 # def taskList(request):
@@ -56,90 +148,3 @@ from .models import todoList
 #         return Response("Item Successfully Deleted !!",status=status.HTTP_204_NO_CONTENT)
 #     except:
 #         return Response(status=status.HTTP_404_NOT_FOUND)
-
-class taskCreateView(APIView):
-
-    def post(self,request):
-        serializer = taskSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-class taskListView(APIView):
-    
-    def get(self,request):
-        tasks = todoList.objects.all().order_by('-id')
-        serializer = taskSerializer(tasks, many=True)
-        return Response(serializer.data)         
-    
-class taskDetailView(APIView):
-
-    def get_object(self,pk):
-        try:
-            return todoList.objects.get(id=pk)
-            # serializer = taskSerializer(task, many=False)
-            # return Response(serializer.data,status=status.HTTP_200_OK)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def get(self,request,pk):
-        task = self.get_object(pk)
-        serializer = taskSerializer(task, many=False)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-
-class taskEditView(APIView):
-    def get_object(self,pk):
-        try:
-            return todoList.objects.get(id=pk)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def put(self, request, pk, format=None):
-        task = self.get_object(pk)
-        serializer = taskSerializer(task, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-class taskDeleteView(APIView):
-    def get_object(self,pk):
-        try:
-            return todoList.objects.get(id=pk)
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-    def delete(self, request, pk, format=None):
-        task = self.get_object(pk)
-        task.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-# class documentationCode(APIView):
-#     def get_object(self, pk):
-#         try:
-#             return todoList.objects.get(id=pk)
-#         except:
-#             raise Http404
-
-#     def get(self, request, pk, format=None):
-#         task = self.get_object(pk)
-#         serializer = taskSerializer(task)
-#         return Response(serializer.data)
-
-#     def put(self, request, pk, format=None):
-#         task = self.get_object(pk)
-#         serializer = taskSerializer(task, data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_202_ACCEPTED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-#     def delete(self, request, pk, format=None):
-#         task = self.get_object(pk)
-#         task.delete()
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-
-
